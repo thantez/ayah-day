7d8a3b671d5e1596c93b6a5ee8575473db68bb24
thantez <bakhshi@protonmail.com>
2020-04-01 19:43:22 +0430

    correct font load and waiting for scenic to fix bug

--- lib/scenes/home.ex
+++ lib/scenes/home.ex
@@ -14,8 +14,10 @@ defmodule AyahDay.Scene.Home do
   @tafsirs_path (@priv_dir |> Path.join("/static/tafsir/")) <> "/"
   @translates_path (@priv_dir |> Path.join("/static/translate/")) <> "/"
 
-  @font_path :code.priv_dir(:ayah_day) |> Path.join("/static/Vazir.ttf")
-  @font_hash Scenic.Cache.Support.Hash.file!(@font_path, :sha)
+  @font_folder :code.priv_dir(:ayah_day) |> Path.join("/static/font")
+  @font_hash "HEyTCnX7BvCOpQ7rN7gSQ5hpqq1NmJReq_9NVj7AbOI"
+  @custom_metrics_path @priv_dir |> Path.join("/static/font/Vazir/Vazir.ttf.metrics")
+  @custom_metrics_hash Scenic.Cache.Support.Hash.file!(@custom_metrics_path, :sha)
 
   # ============================================================================
   # setup
@@ -44,14 +46,19 @@ defmodule AyahDay.Scene.Home do
         image_path = @images_path <> verse_key <> ".png"
         image_hash = Scenic.Cache.Support.Hash.file!(image_path, :sha)
 
-        Scenic.Cache.Static.Font.load(@font_path, @font_hash)
+        Scenic.Cache.Static.Font.load(@font_folder, @font_hash)
+        Scenic.Cache.Static.FontMetrics.load(@custom_metrics_path, @custom_metrics_hash)
         Scenic.Cache.Static.Texture.load(image_path, image_hash)
 
         graph =
           Graph.build()
           |> group(fn g ->
             g
-            |> text(translate, t: {0, 700}, fill: :white, text_align: :center, font: @font_hash)
+            |> text("متن نمونه",
+              t: {0, 700},
+              fill: :white,
+              font: @custom_metrics_hash
+            )
             |> rect({800, 605}, fill: :white)
             |> rect({800, 605}, fill: {:image, {image_hash, 0, 0, 800, 605, 0, 255}})
           end)
--- mix.exs
+++ mix.exs
@@ -27,7 +27,8 @@ defmodule AyahDay.MixProject do
       {:scenic_driver_glfw, "~> 0.10", targets: :host},
       {:httpoison, "~> 1.6"},
       {:jason, "~> 1.2"},
-      {:mogrify, "~> 0.7.3"}
+      {:mogrify, "~> 0.7.3"},
+      {:font_metrics, "~> 0.3.1"}
     ]
   end
 end
